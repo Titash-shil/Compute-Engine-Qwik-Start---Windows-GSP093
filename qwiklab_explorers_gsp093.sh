31233f5a55b119de53378e8b5ab31ae95dc2f3e0
QwikLab Explorers <155870248+Titash-shil@users.noreply.github.com>
2025-04-22 18:47:02 +0530

    Create qwiklab_explorers_gsp093.sh

---
+++ qwiklab_explorers_gsp093.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+clear
+
+
+# Instruction to check authentication
+gcloud auth list
+
+# Instruction to set the zone
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+# Instruction to create the instance
+echo "${CYAN_TEXT}${BOLD_TEXT}Step 3:${RESET_FORMAT} ${WHITE_TEXT}Creating a new Compute Engine instance named 'QwikLab-Explorers'.${RESET_FORMAT}"
+gcloud compute instances create QwikLab-Explorers --project=$DEVSHELL_PROJECT_ID --zone $ZONE --machine-type=e2-medium --create-disk=auto-delete=yes,boot=yes,device-name=QwikLab-Explorers,image=projects/windows-cloud/global/images/windows-server-2022-dc-v20230913,mode=rw,size=50,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced 
+
+# Instruction to wait for the instance to initialize
+sleep 30
+
+# Instruction to get serial port output
+gcloud compute instances get-serial-port-output QwikLab-Explorers --zone=$ZONE
+
+# Instruction to reset the Windows password
+gcloud compute reset-windows-password QwikLab-Explorers --zone $ZONE --user admin --quiet
+
+# Completion message
+
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe my Channel (QwikLab Explorers):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@qwiklabexplorers${RESET_FORMAT}"
